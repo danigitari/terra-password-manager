2920cc3096d0ece7f376643a6b2f7379dc2ec71c
unknown <danigitari>
2023-09-19 01:48:50 +0300

    edited app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,26 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
-class AppServiceProvider extends ServiceProvider
-{
-    /**
+
+  /**
      * Register any application services.
      */
-    public function register(): void
+    class AppServiceProvider extends ServiceProvider
     {
-        //
+        
+        /**
+         * Bootstrap any application services.
+         *
+         * @return void
+         */
+        public function boot()
+        {
+            if($this->app->environment('production')) {
+                URL::forceScheme('https');
+            }
+        }
     }
+    
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
-    }
-}
